6a2b3bb9bd237b6ff357269f1a366b4b0cea2ab3
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-02-07 10:06:05 -0300

    basic settings

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export default wordsList = {
+  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+  fruta: ["Banana", "Maçã", "Pêra", "Uva", "Laranja"],
+  corpo: ["Braço", "Perna", "Olhos", "Pescoço", "Orelhas"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+  programação: ["Framework", "Linguagem", "Angular", "React", "Javascript"],
+  alimento: ["Feijão", "Batata", "Leite", "Arroz", "Pão"],
+};
